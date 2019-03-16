aefff0527a2163be1c4984b8e23149f58f1f83a1
Rayhan <rayhan1793@gmail.com>
2019-03-16 12:44:21 +0100

    Mise en place de la page Stat Manque à faire : - Page statistique paysage (layout + traitement) - problème bdd les joueurs ont qu'une seul partie
    Signed-off-by: Rayhan <rayhan1793@gmail.com>

--- app/src/main/java/fr/kounecorp/gamerz/DataBase.java
+++ app/src/main/java/fr/kounecorp/gamerz/DataBase.java
@@ -1,17 +1,19 @@
 package fr.kounecorp.gamerz;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataBase extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "score.db";
 
-    public final String SQL_CREATEJoueur = "CREATE TABLE Joueur (Pseudo VARCHAR(25) PRIMARY KEY)";
+    public final String SQL_CREATEJoueur = "CREATE TABLE Joueur (Pseudo VARCHAR(25) PRIMARY KEY);";
     public final String SQL_CREATEPartie = "CREATE TABLE Partie (Pseudo VARCHAR(25) NOT NULL," +
             "DateHeure DATETIME NOT NULL," +
             "tempsReactTime INT,"+
@@ -102,6 +104,103 @@ public class DataBase extends SQLiteOpenHelper {
 
         return time;
     }
+
+    public List<String> getAllPseudos() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT Pseudo FROM Joueur ORDER BY Pseudo";
+        List<String> pseudos = new ArrayList<>();
+        pseudos.add("");
+        Cursor c = db.rawQuery(query, null);
+        if (c != null ) {
+            if  (c.moveToFirst()) {
+                do {
+                    String pseudo = c.getString(c.getColumnIndex("Pseudo"));
+                    pseudos.add(pseudo);
+                }while (c.moveToNext());
+            }
+            c.close();
+        }
+        return pseudos;
+    }
+
+    public int getBestScore(String jeu, String pseudo) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = (pseudo.isEmpty()) ?
+                "SELECT " + jeu + " FROM Partie ORDER BY 1 DESC" :
+                "SELECT " + jeu + " FROM Partie WHERE pseudo = \'" + pseudo + "\'ORDER BY 1 DESC";
+        Cursor data = db.rawQuery(query, null);
+        int score = 0;
+        if (data.moveToFirst()) {
+            score = data.getInt(0);
+        }
+        data.close();
+        return score;
+    }
+
+    public int getBestReactTime(String pseudo) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = (pseudo.isEmpty()) ?
+                "SELECT tempsReactTime FROM Partie ORDER BY 1 ASC" :
+                "SELECT tempsReactTime FROM Partie WHERE pseudo = \'" + pseudo + "\'ORDER BY 1 ASC";
+        Cursor data = db.rawQuery(query, null);
+        int score = 0;
+        if (data.moveToFirst()) {
+            score = data.getInt(0);
+        }
+        data.close();
+        return score;
+    }
+
+    public double getBestTime(String pseudo) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = (pseudo.isEmpty()) ?
+                "SELECT tempsFormClick " +
+                "FROM Partie " +
+                "WHERE scoreFormClick = " + getBestScore("scoreFormClick",pseudo) +
+                " ORDER BY 1 DESC" : ////////////////////////////////
+                "SELECT tempsFormClick " +
+                "FROM Partie " +
+                "WHERE pseudo = \'" + pseudo + "\' " +
+                "AND scoreFormClick = " + getBestScore("scoreFormClick",pseudo) +
+                " ORDER BY 1 DESC";
+        Cursor data = db.rawQuery(query, null);
+        double time = 0;
+        if (data.moveToFirst()) {
+            time = data.getDouble(0);
+        }
+        data.close();
+        return time;
+    }
+
+    public int getScoreMoy(String jeu, String pseudo) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = (pseudo.isEmpty()) ?
+                "SELECT AVG("+jeu+") FROM Partie":
+                "SELECT AVG("+jeu+"), Pseudo FROM Partie WHERE Pseudo = \'" + pseudo + "\' GROUP BY Pseudo";
+        Cursor data = db.rawQuery(query, null);
+        int score = 0;
+        if (data.moveToFirst()) {
+            score = data.getInt(0);
+        }
+        data.close();
+        return score;
+    }
+
+    public double getTempsMoy(String pseudo) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = (pseudo.isEmpty()) ?
+                "SELECT AVG(tempsFormClick) FROM Partie" :
+                "SELECT AVG(tempsFormClick), Pseudo FROM Partie WHERE pseudo = \'" + pseudo + "\' GROUP BY Pseudo";
+        Cursor data = db.rawQuery(query, null);
+        double score = 0;
+        if (data.moveToFirst()) {
+            score = data.getDouble(0);
+        }
+        data.close();
+        return score;
+    }
+
+
 
 
 }
--- app/src/main/java/fr/kounecorp/gamerz/Statistique.java
+++ app/src/main/java/fr/kounecorp/gamerz/Statistique.java
@@ -1,19 +1,104 @@
 package fr.kounecorp.gamerz;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
 
 public class Statistique extends AppCompatActivity {
+
+    private TextView statReact, statFormClick, statFastClicker;
+    private Spinner spinnerPseudo;
+    private RadioGroup radioGroup;
+
+    private DataBase db;
+
+    private NumberFormat format;
+    private String currentPseudo;
+    private boolean highScore = true;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stat);
+
+        format = NumberFormat.getInstance(Locale.US);
+        format.setMinimumFractionDigits(2);
+        format.setMaximumFractionDigits(3);
+
+        db = new DataBase(this);
+
+        spinnerPseudo = findViewById(R.id.spinnerPseudo);
+        List<String> pseudos = db.getAllPseudos();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.pseudos_spinnerview, R.id.pseudoInSpinner, pseudos);
+        spinnerPseudo.setAdapter(adapter);
+
+        radioGroup = findViewById(R.id.radioGroupStat);
+
+        statReact = findViewById(R.id.statReactTime);
+        statFormClick = findViewById(R.id.statFormClick);
+        statFastClicker = findViewById(R.id.statFastClicker);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                highScore = (checkedId == R.id.radioButtonHighScore);
+                updateView(currentPseudo, highScore);
+            }
+        });
+
+        spinnerPseudo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currentPseudo = (String) parent.getItemAtPosition(position);
+                updateView(currentPseudo, highScore);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void updateView(String pseudo, boolean highScore) {
+        String reactime, formClick, fastClicker;
+        if (highScore) {
+            reactime = getString(R.string.reactTimeHS, db.getBestReactTime(pseudo));
+            formClick = getString(R.string.formClickHS, db.getBestScore("scoreFormClick", pseudo), format.format(db.getBestTime(pseudo)));
+            fastClicker = getString(R.string.fastClickerHS, db.getBestScore("scoreFastClicker",pseudo));
+        } else {
+            reactime = getString(R.string.reactTimeMOY, db.getScoreMoy("tempsReactTime",pseudo));
+            formClick = getString(R.string.formClickMOY, db.getScoreMoy("scoreFormClick", pseudo), format.format(db.getTempsMoy(pseudo)));
+            fastClicker = getString(R.string.fastClickerMOY, db.getScoreMoy("scoreFastClicker",pseudo));
+        }
+        statReact.setText(Html.fromHtml(reactime));
+        statFormClick.setText(Html.fromHtml(formClick));
+        statFastClicker.setText(Html.fromHtml(fastClicker));
     }
 
     @Override
